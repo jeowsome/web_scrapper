ba13eeda8132ac642459f1be9f40419935eaac7a
jeowsome <jeowsome15@gmail.com>
2023-04-25 18:10:16 +0800

    Web Scraper 2/5 - Parsing HTML

--- Web Scraper/task/scraper.py
+++ Web Scraper/task/scraper.py
@@ -1,16 +1,36 @@
 import requests
+from bs4 import BeautifulSoup
 
-INVALID_REQUEST = "Invalid quote resource!"
+INVALID_REQUEST = "Invalid page!"
 
 
-def process_request(url):
+def get_request(url):
     r = requests.get(url)
 
     if r:
-        result = r.json()
-        return result.get("content", INVALID_REQUEST)
+        return r.content
+    return INVALID_REQUEST
+
+
+def process_result(get_result):
+    result = {}
+    if get_result == INVALID_REQUEST:
+        return get_result
+
+    soup = BeautifulSoup(get_result, 'html.parser')
+
+    if soup.find('title').text and soup.select('meta[name="description"]'):
+        result.update({
+            'title': soup.find('title').text,
+            'description': soup.select_one('meta[name="description"]').get('content')
+        })
+
+    if result.get('title') and result.get('description'):
+        return result
     return INVALID_REQUEST
 
 
 input_url = input("Input the URL:\n")
-print(process_request(input_url))
+res = get_request(input_url)
+print(process_result(res))
+
