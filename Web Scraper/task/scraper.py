b9f07f38a0277c4d1556d5ca1e3da8c4420a3b14
jeowsome <jeowsome15@gmail.com>
2023-04-28 01:41:06 +0800

    5 / 5 - Web Scrapper Completed

--- Web Scraper/task/scraper.py
+++ Web Scraper/task/scraper.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import string
-
+import os
 
 def remove_punctuations(word: string) -> string:
     return word.translate(str.maketrans('', '', string.punctuation))
@@ -24,20 +24,28 @@ def write_content(filename, path):
     file_list.append(filename)
 
 
+CWD = os.getcwd()
 URL = string.Template("https://www.nature.com${path}")
+REL_PATH = string.Template("/nature/articles?sort=PubDate&year=2020&page=${page}")
 PARSER_OPTION = "html.parser"
-r = requests.get(URL.substitute(path="/nature/articles?sort=PubDate&year=2020&page=3"))
-soup = BeautifulSoup(r.content, PARSER_OPTION)
-article_list = soup.find_all("article")
 file_list = []
+pages = int(input())
+keyword = input()
 
-for article in article_list:
-    meta = article.select_one("div > span[data-test='article.type'] > span.c-meta__type")
+for i in range(1, pages + 1):
+    new_folder = f"PAGE_{i}"
+    os.mkdir(new_folder)
+    os.chdir(new_folder)
+    r = requests.get(URL.substitute(path=REL_PATH.substitute(page=i)))
+    soup = BeautifulSoup(r.content, PARSER_OPTION)
+    article_list = soup.find_all("article")
 
-    if meta.text == 'News':
-        title_container = article.select_one('[data-track-action="view article"]')
-        title = get_filename(title_container.text)
-        write_content(filename=title, path=title_container.get('href'))
+    for article in article_list:
+        meta = article.select_one("div > span[data-test='article.type'] > span.c-meta__type")
+        if meta.text == keyword:
+            title_container = article.select_one('[data-track-action="view article"]')
+            title = get_filename(title_container.text)
+            write_content(filename=title, path=title_container.get('href'))
+    os.chdir(CWD)
 
-
-print("Saved articles:", file_list)
+print("Saved all articles.")
